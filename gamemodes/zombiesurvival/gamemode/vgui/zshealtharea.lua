f89dc25db13684b6af3653aa1c18d0f03b8fa7c0
Flairieve <flairieve@gmail.com>
2018-02-02 23:20:01 -0600

    Health GUI Adjustments

--- gamemodes/zombiesurvival/gamemode/vgui/zshealtharea.lua
+++ gamemodes/zombiesurvival/gamemode/vgui/zshealtharea.lua
@@ -83,12 +83,12 @@ end
 function PANEL:PerformLayout()
 	local screenscale = BetterScreenScale()
 
-	self:SetSize(screenscale * 350, screenscale * 128)
+	self:SetSize(screenscale * 350, screenscale * 168)
 
-	self.HealthModel:SetWide(self:GetTall())
+	self.HealthModel:SetWide(self:GetTall() - 28)
 
 	self:AlignLeft(screenscale * 24)
-	self:AlignBottom(screenscale * 24)
+	self:AlignBottom(screenscale * 2)
 end
 
 function PANEL:Paint()
